6fbca21966dbaf41804c6d5dc9748795cb746a19
sagar mahapatro <sagarmahapatro@gmail.com>
2014-01-08 21:21:52 +0530

    adding some more files

---
+++ WebContent/testDataSource.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="sql" uri="http://java.sun.com/jsp/jstl/sql"%>
+
+<sql:query var="result" dataSource="jdbc/affablebean">
+    SELECT * FROM category, product
+    WHERE category.id = product.category_id
+</sql:query>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<table border="1">
+    <!-- column headers -->
+    <tr>
+        <c:forEach var="columnName" items="${result.columnNames}">
+            <th><c:out value="${columnName}"/></th>
+        </c:forEach>
+    </tr>
+    <!-- column data -->
+    <c:forEach var="row" items="${result.rowsByIndex}">
+        <tr>
+            <c:forEach var="column" items="${row}">
+                <td><c:out value="${column}"/></td>
+            </c:forEach>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
---
+++ src/com/affableBean/controller/ControllerServlet.java
@@ -0,0 +1,87 @@
+package com.affableBean.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControllerServlet
+ */
+
+public class ControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userPath = request.getServletPath();
+		String contextPath = request.getContextPath();
+        // if category page is requested
+        if (userPath.equals("/category")) {
+            // TODO: Implement category request
+        	
+        	
+        // if cart page is requested
+        } else if (userPath.equals("/viewCart")) {
+            // TODO: Implement cart page request
+
+            userPath = "/cart";
+
+        // if checkout page is requested
+        } else if (userPath.equals("/checkout")) {
+            // TODO: Implement checkout page request
+
+        // if user switches language
+        } else if (userPath.equals("/chooseLanguage")) {
+            // TODO: Implement language request
+
+        }
+        
+       // use RequestDispatcher to forward request internally
+        String url = "/WEB-INF/view" +userPath+ ".jsp";
+        System.out.println("url>>>>>>>>>>"+url);
+        try {
+            request.getRequestDispatcher(url).forward(request, response);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userPath = request.getServletPath();
+
+        // if addToCart action is called
+        if (userPath.equals("/addToCart")) {
+            // TODO: Implement add product to cart action
+
+        // if updateCart action is called
+        } else if (userPath.equals("/updateCart")) {
+            // TODO: Implement update cart action
+
+        // if purchase action is called
+        } else if (userPath.equals("/purchase")) {
+            // TODO: Implement purchase action
+
+            userPath = "/confirmation";
+        }
+
+        // use RequestDispatcher to forward request internally
+        String url = "/WEB-INF/view" + userPath + ".jsp";
+
+        try {
+            request.getRequestDispatcher(url).forward(request, response);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+	}
+
+}
